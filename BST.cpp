b75e2b60591cc8a62cb7bdef23dbd9b09d12d927
Vighnesh <156537424+vighnesh-7@users.noreply.github.com>
2024-09-26 14:41:01 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -80,12 +80,80 @@ public:
 
         if(root->val <= min || root->val >= max) {
             return false;
-        }        if(root->left == NULL && root->right == NULL) {
+        }
+        if(root->left == NULL && root->right == NULL) {
             return true;
         }
 
         return isValidBst(root->left,min,root->val) && isValidBst(root->right,root->val,max);
     }
+
+
+
+
+    Node* findLastRight(Node* root) {
+        if(root == NULL) {
+            return NULL;
+        }
+
+        while(root->right != NULL) {
+            root = root->right;
+        }
+
+        return root;
+    }
+
+    Node* merger(Node* root){
+        if(root->left == NULL) {
+            return root->right;
+        }
+        else if(root->right == NULL) {
+            return root->left;
+        }
+
+        Node* rightChild = root->right;
+        Node* lastRight = findLastRight(root->left);
+        lastRight->right = rightChild;
+
+        return root->left;
+    }
+
+
+
+    Node* deleteNode(Node* root,int key) {
+        if(root == NULL) {
+            return NULL;
+        }
+
+        if(root->val == key) {
+            return merger(root);
+        }
+
+        Node* curr = root;
+        while(curr != NULL) {
+            if(curr->val > key) {
+                if(curr->left != NULL && curr->left->val == key) {
+                    curr->left = merger(curr->left);
+                    break;
+                }
+                else {
+                    curr = curr->left;
+                }
+            }
+            else {
+                if(curr->right != NULL && curr->right->val == key) {
+                    curr->right = merger(curr->right);
+                    break;
+                }
+                else {
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return root;
+    }
+
 
 };
 
@@ -94,8 +162,8 @@ class BstIterator {
 private:
     stack<Node*> st;
     bool reverse = true;
-    // reverse = true -> before pointer // inorder predecessor
-    // reverse = false -> next pointer // inorder successor
+    // reverse = true -> before pointer // inorder predecessor // largest element
+    // reverse = false -> next pointer // inorder successor // smallest element 
 
 public:
     BstIterator(Node* root,bool isReverse) {
